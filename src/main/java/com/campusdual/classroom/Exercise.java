1e4f056019605eed3b01365a5b7c9b6e2eb973e4
Paola Holguin Giraldo <pao.hogi@gmail.com>
2025-10-21 17:23:14 +0200

    Solved Exercise 5

--- src/main/java/com/campusdual/classroom/Exercise.java
+++ src/main/java/com/campusdual/classroom/Exercise.java
@@ -18,8 +18,20 @@ public class Exercise {
     // Si la hora es desde las 14:00 a las 20:59, imprimir por pantalla → It's afternoon
     // Si la hora es desde las 21:00 a las 23:59, imprimir por pantalla → It's night
     public static void checkHour(int hour) {
+        if (hour >= 0 && hour < 7) {
+            System.out.println("It's night");
+        } else if (hour >= 7 && hour < 14) {
+            System.out.println("It's morning");
+        } else if (hour >= 14 && hour < 21) {
+            System.out.println("It's afternoon");
+        } else if (hour >= 21 && hour < 24) {
+            System.out.println("It's night");
+        } else {
+            System.out.println("Invalid hour");
+        }
 
     }
+
 
     //TODO ↓
     // Si es Enero, Febrero o Marzo, imprimir por pantalla → Winter
@@ -28,6 +40,30 @@ public class Exercise {
     // Si es Octubre, Noviembre o Diciembre, imprimir por pantalla → Autumn
     public static void checkSeason(int month) {
 
+        switch (month) {
+            case 1:
+            case 2:
+            case 3:
+                System.out.println("Winter");
+                break;
+            case 4:
+            case 5:
+            case 6:
+                System.out.println("Spring");
+                break;
+            case 7:
+            case 8:
+            case 9:
+                System.out.println("Summer");
+                break;
+            case 10:
+            case 11:
+            case 12:
+                System.out.println("Autumn");
+                break;
+            default:
+                System.out.println("Invalid month");
+                break;
+        }
     }
-
 }
